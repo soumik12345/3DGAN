f096d08d61aeae70e70f7f3717831575713a27e2
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2019-11-19 04:46:47 +0530

    Added Doctring for Training function

--- src/training.py
+++ src/training.py
@@ -4,7 +4,14 @@ import numpy as np
 
 
 def train(generator, discriminator, gan, x_train):
-    gen_loss_history, discriminator_loss_history = [], []
+    '''Train GAN
+    Params:
+        generator       -> Generator Model
+        discriminator   -> Discriminator Model
+        gan             -> Combined GAN Model
+        x_train         -> Training Data
+    '''
+    generator_loss_history, discriminator_loss_history = [], []
     for epoch in tqdm(range(1, EPOCHS + 1)):
         index = np.random.randint(len(x_train), size=BATCH_SIZE)
         real = x_train[index]
@@ -24,3 +31,4 @@ def train(generator, discriminator, gan, x_train):
         z = np.random.normal(0, 0.33, size=[BATCH_SIZE, 1, 1, 1, LATENT_DIMENSION]).astype(np.float32)
         generator_loss = gan.train_on_batch(z, np.reshape([1] * BATCH_SIZE, (-1, 1, 1, 1, 1))).astype(np.float64)
         generator_loss_history.append(generator_loss)
+    return generator_loss_history, discriminator_loss_history
