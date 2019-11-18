fd67c2c9c07caf7543cad843f36c3130fd90ffb6
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2019-11-19 02:44:05 +0530

    Added Dataset

---
+++ src/dataset.py
@@ -0,0 +1,35 @@
+import numpy as np
+from glob import glob
+from tqdm import tqdm
+from scipy.io import loadmat
+from os.path import join, dirname
+from tensorflow.keras.utils import get_file
+
+
+def download_existing_dataset(dataset_url, dataset_name):
+    '''Download Dataset
+    Params:
+        dataset_url     -> Url to the dataset
+        dataset_name    -> Name of the dataset
+    '''
+    file_name = dataset_url.split('/')[-1]
+    path = get_file(
+        file_name,
+        origin=dataset_url,
+        extract=True
+    )
+    path = join(dirname(path), dataset_name)
+    return path
+
+
+def load_data(dataset_path):
+    '''Load Voxel Data
+    Params:
+        dataset_path -> Path to the dataset
+    '''
+    x = []
+    mat_files = glob(join(dataset_path, '*.mat'))
+    for _file in tqdm(mat_files):
+        data = loadmat(_file)
+        x.append(data['voxel'])
+    return x_train
